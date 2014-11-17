d8f2979c545bec6e7f1a2d1bc9ddda2e9c28a8b8
FrederikOlesen <Frederik.o@mailme.dk>
2014-11-17 10:43:22 +0100

    Started to work on implementing the API part.

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -2,12 +2,13 @@
 
 angular.module('myAppRename.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: '/partials/partial1',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: '/partials/partial1',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', function() {
-});
+    .controller('View1Ctrl', function () {
+        
+    });
